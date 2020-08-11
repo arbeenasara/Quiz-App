bf7036503c22a824a593ba2465d10506ad1a858e
arbeenasara <arbeena27memon@gmail.com>
2020-08-11 18:18:38 +0500

    Quiz App

---
+++ question.js
@@ -0,0 +1,141 @@
+var questions = [{
+    "question": "The correct form of Accounting equation is:",
+    "option1": "Assets + Liabilities = Equity",
+    "option2": "Assets - Liabilities = Equity",
+    "option3": "Assets - Receivable = Equity",
+    "option4": "Assets + Receivable = Equity",
+    "answer": "2"
+}, {
+    "question": "Withdrawal of inventory by the owner for personal use appears in trading account as a deduction from:",
+    "option1": "sales",
+    "option2": "purchases",
+    "option3": "overdrafts",
+    "option4": "none of these",
+    "answer": "2"
+}, {
+    "question": "What standards are used to prepare financial statements by most of the countries and companies?",
+    "option1": "International Financial Reporting Standards",
+    "option2": "International Financial Accounting Standards",
+    "option3": "International Accounting & Auditing Standards",
+    "option4": "International Risk Reporting Standards",
+    "answer": "1"
+}, {
+    "question": "Advance payments are recognized as:",
+    "option1": "receivable",
+    "option2": "payable",
+    "option3": "bad debt",
+    "option4": "none of these",
+    "answer": "1"
+}, {
+    "question": "What from the following is NOT a current asset?",
+    "option1": "Patents",
+    "option2": "Inventory",
+    "option3": "Cash",
+    "option4": "Trade receivables",
+    "answer": "1"
+}, {
+    "question": "Depreciable amount =",
+    "option1": "Cost of an asset + Residual value",
+    "option2": "Cost of an asset - Residual value",
+    "option3": "Residual value - Cost of an asset",
+    "option4": "None of these",
+    "answer": "2"
+}, {
+    "question": "What from the following is NOT a non-current asset?",
+    "option1": "Capital",
+    "option2": "Property",
+    "option3": "Patents",
+    "option4": "Inventory",
+    "answer": "4"
+}, {
+    "question": "The accounting process of allocation cost of intangible assets is called:",
+    "option1": "Amortization",
+    "option2": "Depletion",
+    "option3": "Going Concern",
+    "option4": "Residual Value",
+    "answer": "1"
+}, {
+    "question": "What from the following is NOT a capital expense?",
+    "option1": "Purchase of property",
+    "option2": "Purchase of office equipment",
+    "option3": "Replacement of a vehicle",
+    "option4": "Repair of a vehicle",
+    "answer": "4"
+}, {
+    "question": "Depreciation is normally charged as:",
+    "option1": "payable",
+    "option2": "receivable",
+    "option3": "expenses",
+    "option4": "advances",
+    "answer": "3"
+}, {
+    "question": "Raw materials that are remaining at the end of the reporting period are treated as:",
+    "option1": "liabilities",
+    "option2": "expenses",
+    "option3": "fixed assets",
+    "option4": "current assets",
+    "answer": "4"
+}, {
+    "question": "The goods that have not been sold to the customers till the end of the reporting period are considered as",
+    "option1": "inventories",
+    "option2": "expenses",
+    "option3": "sales",
+    "option4": "purchases",
+    "answer": "1"
+}, {
+    "question": "The goods that have been sold to the customers are treated as ____ in the financial statements.",
+    "option1": "inventories",
+    "option2": "expenses",
+    "option3": "income",
+    "option4": "debt",
+    "answer": "2"
+}, {
+    "question": "A bank overdraft is shown as a/an ____ balance in the bank statement.",
+    "option1": "debit",
+    "option2": "credit",
+    "option3": "credit - debit",
+    "option4": "none of these",
+    "answer": "1"
+}, {
+    "question": "Which from the following is included in the cost of purchases?",
+    "option1": "Abnormal loss",
+    "option2": "Freight in",
+    "option3": "Rent of store",
+    "option4": "Administrative Salaries",
+    "answer": "2"
+}, {
+    "question": "Any transaction which cannot be recorded in any book of prime entry is recorded in:",
+    "option1": "Cash book",
+    "option2": "Petty cash book",
+    "option3": "General journal",
+    "option4": "Day books",
+    "answer": "3"
+}, {
+    "question": "Cheques issued but not presented, cause the bank statement balance to be ____ the cash book balance.",
+    "option1": "greater than",
+    "option2": "less than",
+    "option3": "equal to",
+    "option4": "the two statements are irrelevant",
+    "answer": "1"
+}, {
+    "question": "The capital of a business is Rs.100,000 and the liabilities are Rs.40,000. What are the total assets?",
+    "option1": "Rs.40,000",
+    "option2": "Rs.60,000",
+    "option3": "Rs.100,000",
+    "option4": "Rs.140,000",
+    "answer": "4"
+}, {
+    "question": "A company had opening inventory of Rs.200,000. Sales and purchases during the period were of Rs.400,000 and Rs.80,000 respectively. What is the gross profit for the period if the closing inventory was worth Rs.100,000?",
+    "option1": "Rs.20,000",
+    "option2": "Rs.120,000",
+    "option3": "Rs.220,000",
+    "option4": "Rs.320,000",
+    "answer": "3"
+}, {
+    "question": "A debit entry usually represents:",
+    "option1": "Assets and Expenses",
+    "option2": "Assets and Income",
+    "option3": "Liabilities and Income",
+    "option4": "Liabilities and Expenses",
+    "answer": "1"
+}];
